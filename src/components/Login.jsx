589a304c0594071b475e6a70156074d67011845a
viniseverodev <yvinisevero@gmail.com>
2025-08-13 00:17:24 -0300

    Adicionando estilo para tela de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,23 @@
+import '../css/login.css'
+
 function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
+
+
+
+
+  return ( 
+    <div className='container cardForm'>
+      <form className='form'>
+        <label htmlFor="email">Email</label>
+        <input type="text" placeholder='Digite seu e-mail'/>
+
+        <label htmlFor="password">Senha</label>
+        <input type="password" placeholder='Digite sua senha' />
+
+        <button className='btn'>Entrar</button>
+
+        <p className='lostPass'>Esqueceu a senha?</p>
+      </form>
     </div>
   );
 }
