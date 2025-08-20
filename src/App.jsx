577475e7ca7328ec88706915b2f617986c50fd7b
NicolasS-L <nicolasloeblein73@gmail.com>
2025-08-20 20:46:40 -0300

    tela de registro finalizada (#4)
    Co-authored-by: LUCAS AREND MACIEL <54964204+LucasArend@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Transacao from "./components/transacao";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Transacao from "./components/Transacao";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Menu from "./Menu";
+import Register from "./components/Register";
 import { AuthProvider } from "./context/AuthContext";
+
+function AppLayout() {
 
 
 
 function App() {
+   const location = useLocation();
+    const hideMenu =
+      location.pathname === "/" ||
+      location.pathname === "/login" ||
+      location.pathname === "/register";
   return (
+    <Router>
+      <AppLayout />
     <AuthProvider>
       <Router>
         <Menu />
+        <>
+        {!hideMenu && <Menu />}
         <Routes>
-          <Route path="/" element={<Login />} /> 
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/transacao" element={<Transacao />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-    </Router>
+      </Router>
     </AuthProvider>
   );
 }
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,7 +4,9 @@ import { Navigate } from 'react-router-dom'
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth()
 
+
   return user ? children : <Navigate to='/login' />
+
 }
 
 export default PrivateRoute
