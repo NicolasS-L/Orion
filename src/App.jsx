096c951602c9b53f021d7db7ae01338a06c4c374
Lucas Arend Maciel <lucas.m.arend@gmail.com>
2025-08-20 22:08:54 -0300

    tipo(escopo): descrição breve

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Transacao from "./components/Transacao";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import Menu from "./Menu";
 import Register from "./components/Register";
 import { AuthProvider } from "./context/AuthContext";
-
-function AppLayout() {
-
-
+import Transacao from "./components/transacao";
 
 function App() {
-   const location = useLocation();
-    const hideMenu =
-      location.pathname === "/" ||
-      location.pathname === "/login" ||
-      location.pathname === "/register";
+
   return (
-    <Router>
-      <AppLayout />
     <AuthProvider>
       <Router>
-        <Menu />
-        <>
-        {!hideMenu && <Menu />}
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/transacao" element={<Transacao />} />
